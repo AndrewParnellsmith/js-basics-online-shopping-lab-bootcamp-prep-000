f5dcd328712257dc3160c93fdd1903b06c9a8114
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-07-08 02:56:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,12 +46,12 @@ for (var i = 0; i < cart.length; i++) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber)
-  { console.log (`Sorry, we don't have a credit card on file for you.`)
+  { return (`Sorry, we don't have a credit card on file for you.`)
   } else {  
-    console.log (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    cart.length = 0
+    return (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    
  } 
- 
+ cart.splice(0,5)
   }
 
         
